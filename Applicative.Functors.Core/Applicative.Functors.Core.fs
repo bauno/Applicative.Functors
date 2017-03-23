fa12eb0f1817efa010c3f8acb71b2bc5ee0159a8
Alessandro Bonometti <bauno@bauniga.baita>
2017-03-23 18:55:13 +0100

    Added customer validation

--- Applicative.Functors.Core/Applicative.Functors.Core.fs
+++ Applicative.Functors.Core/Applicative.Functors.Core.fs
@@ -1,4 +1,25 @@
-namespace Applicative.Functors.Core
+module Applicative.Core
 
-type Dummy() = 
-    member this.X = "F#"
+open Chessie.ErrorHandling
+open System.Text.RegularExpressions
+
+type Customer = {Id: int; Name: string; Email: string}
+
+let validateId id = if id > 0 then ok id else fail "Invalid Id"
+
+let validateName (name: string) = if name.Length > 2 then ok name else fail "name too short"
+
+let validateEmail (email: string) = 
+    let regex = Regex @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    match regex.IsMatch(email) with
+    | true -> ok email
+    | false -> fail "email not valid"
+    
+
+let createCustomer id name email =
+    let create = fun i n e -> {Id = i; Name = n; Email = e}
+
+    create
+    <!> validateId id
+    <*> validateName name
+    <*> validateEmail email
--- Applicative.Functors.Tests/Applicative.Functors.Tests.fs
+++ Applicative.Functors.Tests/Applicative.Functors.Tests.fs
@@ -4,6 +4,7 @@ open Xunit
 open FsUnit.Xunit
 open System.Linq
 open Chessie.ErrorHandling
+open Applicative.Core
 
 [<Fact>]
 let ``lift should multiply inner value of success by 2``() =
@@ -14,8 +15,13 @@ let ``lift should multiply inner value of success by 2``() =
 let extractError =
     function
     | Ok(n, _) -> failwith "Error: I was expecting an error :-)"
-    | Bad s -> s.[0]
+    | Bad s -> s
 
+let extractOk =
+    function
+    | Ok(n,_) -> n
+    | Bad _ -> failwith "Error: I was expecting ok!"
+    
 let doublex x =
      x * 2
 
@@ -32,13 +38,50 @@ let ``lift applied to a failure should fail``() =
     fail "error"
     |> Trial.lift doublex
     |> extractError
-    |> should equal "error"
+    |> should equal ["error"]
 
 [<Fact>]
 let ``lift applied to a failure using infix operator should fail``() =
-    doublex <!> fail "Infix error"
+    doublex <!> fail "Infix error"    
     |> extractError
-    |> should equal "Infix error"
+    |> should equal ["Infix error"]
    
 
+[<Fact>]
+let ``bind should bypass after first error``() = 
+    let validate c = ok c
+    let update c = fail "update error"
+    let send c = fail "send error"
 
+    let customer = { Id = 42; Name = "John"; Email = "john@example.com" }
+
+    validate customer
+    >>= update
+    >>= send
+    |> extractError
+    |> should equal ["update error"]
+
+[<Fact>]
+let ``bind should create valid customer if no failures``() = 
+    let validate c = ok c
+    let update c = ok c
+    let send c = ok c
+
+    let customer = { Id = 42; Name = "John"; Email = "john@example.com" }
+
+    validate customer
+    >>= update
+    >>= send
+    |> should equal (ok customer)  
+
+[<Fact>]
+let ``apply valid inputs should create valid customer``() = 
+    createCustomer 42 "John" "john@example.com"
+    |> extractOk
+    |> should equal { Id = 42; Name = "John"; Email = "john@example.com" }
+
+[<Fact>]
+let ``apply invalid inputs should fail with accumulated messages``() = 
+    createCustomer -1 "John" "foo"
+    |> extractError
+    |> should equal [ "Invalid id"; "email not valid" ]
