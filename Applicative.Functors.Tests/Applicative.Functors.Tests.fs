2a4e6ec48b735e7be0e9d1d0892b3dc427083eda
bauno <a.bonometti@gmail.com>
2017-08-14 15:29:22 +0200

    Fixed an incredibly stupid typo (but learned a lot :)

--- Applicative.Functors.Tests/Applicative.Functors.Tests.fs
+++ Applicative.Functors.Tests/Applicative.Functors.Tests.fs
@@ -1,15 +1,16 @@
-module Applicative.Functors.Tests
+ module Applicative.Functors.Tests
 
-open Xunit 
+open Xunit
 open FsUnit.Xunit
 open System.Linq
 open Chessie.ErrorHandling
 open Applicative.Core
+open Swensen.Unquote
 
 [<Fact>]
 let ``lift should multiply inner value of success by 2``() =
     ok 16
-    |> Trial.lift ((*) 2) 
+    |> Trial.lift ((*) 2)
     |> should equal (ok 32)
 
 let extractError =
@@ -21,7 +22,7 @@ let extractOk =
     function
     | Ok(n,_) -> n
     | Bad _ -> failwith "Error: I was expecting ok!"
-    
+
 let doublex x =
      x * 2
 
@@ -42,13 +43,13 @@ let ``lift applied to a failure should fail``() =
 
 [<Fact>]
 let ``lift applied to a failure using infix operator should fail``() =
-    doublex <!> fail "Infix error"    
+    doublex <!> fail "Infix error"
     |> extractError
     |> should equal ["Infix error"]
-   
+
 
 [<Fact>]
-let ``bind should bypass after first error``() = 
+let ``bind should bypass after first error``() =
     let validate c = ok c
     let update c = fail "update error"
     let send c = fail "send error"
@@ -62,7 +63,7 @@ let ``bind should bypass after first error``() =
     |> should equal ["update error"]
 
 [<Fact>]
-let ``bind should create valid customer if no failures``() = 
+let ``bind should create valid customer if no failures``() =
     let validate c = ok c
     let update c = ok c
     let send c = ok c
@@ -72,16 +73,22 @@ let ``bind should create valid customer if no failures``() =
     validate customer
     >>= update
     >>= send
-    |> should equal (ok customer)  
+    |> should equal (ok customer)
 
 [<Fact>]
-let ``apply valid inputs should create valid customer``() = 
+let ``apply valid inputs should create valid customer``() =
     createCustomer 42 "John" "john@example.com"
     |> extractOk
     |> should equal { Id = 42; Name = "John"; Email = "john@example.com" }
 
 [<Fact>]
-let ``apply invalid inputs should fail with accumulated messages``() = 
-    createCustomer -1 "John" "foo"
-    |> extractError
-    |> should equal [ "Invalid id"; "email not valid" ]
+let ``apply invalid inputs should fail with accumulated messages``() =    
+
+    let expected = Bad ([ "Invalid Id"; "email not valid" ]) : Result<Customer, string>
+    let res = createCustomer -1 "John" "foo" 
+    
+    res |> should equal expected
+    test <@ expected  = res@>    
+
+    
+    
